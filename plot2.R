03b7fa839313810f568cdf0fc10899747a0d06ca
Nop287 <norbert.widmann@gmx.de>
2015-02-06 22:51:15 +0100

    Plot 2 ready.

---
+++ plot2.R
@@ -0,0 +1,18 @@
+## Efficient way to read just the relevant data from the data set
+require(sqldf)
+
+plot2 <- function() {
+  ## Select just two days from the data set
+  data <- read.csv.sql("household_power_consumption.txt", 
+                       "SELECT * FROM file WHERE Date = \"1/2/2007\" OR Date = \"2/2/2007\"",
+                       sep = ";")
+
+  ## convert Date and Time to da datatime object for plotting
+  data$Time <- strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+  ## convert Date from string to an actual Date
+  data$Date <- strptime(data$Date, format = "%d/%m/%Y")
+
+  png("plot2.png")
+  plot(data$Time, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+  dev.off()
+}
